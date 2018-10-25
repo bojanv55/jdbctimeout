a43604fe520b2fc876329ba10362db0d0e3d2f21
bojan <bojan@vukas.me>
2018-10-25 23:25:22 +0200

    trying testcontainers

--- src/test/java/me/vukas/jdbc/JdbcTimeoutTests.java
+++ src/test/java/me/vukas/jdbc/JdbcTimeoutTests.java
@@ -1,21 +1,30 @@
 package me.vukas.jdbc;
 
+import static java.time.temporal.ChronoUnit.SECONDS;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.time.temporal.TemporalUnit;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 import eu.rekawek.toxiproxy.Proxy;
 import eu.rekawek.toxiproxy.model.ToxicDirection;
 
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.wait.strategy.HostPortWaitStrategy;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ToxiProxyConfig.class)
@@ -28,6 +37,16 @@ public class JdbcTimeoutTests {
 	private JdbcTemplate jdbcTemplate;
 	@Autowired
 	private Proxy mysqlProxy;
+
+	@ClassRule
+	public static MySQLContainer mysql = new MySQLContainer()
+			.withUsername("root")
+			.withPassword("123456");
+
+	@ClassRule
+	public static GenericContainer toxiProxy =
+			new GenericContainer("shopify/toxiproxy")
+					.withExposedPorts(8474);
 
 	@Test
 	public void readTimeout(){
--- src/test/java/me/vukas/jdbc/ToxiProxyConfig.java
+++ src/test/java/me/vukas/jdbc/ToxiProxyConfig.java
@@ -8,20 +8,23 @@ import java.io.IOException;
 import eu.rekawek.toxiproxy.Proxy;
 import eu.rekawek.toxiproxy.ToxiproxyClient;
 
+import org.junit.ClassRule;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.MySQLContainer;
 
 @TestConfiguration
 public class ToxiProxyConfig {
 
 		@Bean
 		public ToxiproxyClient toxiproxyClient(){
-			return new ToxiproxyClient("10.10.121.219", 8474);
+			return new ToxiproxyClient("192.168.1.2", 8474);
 		}
 
 		@Bean
 		public Proxy mysqlProxy() throws IOException {
-			Proxy proxy = toxiproxyClient().createProxy("mysql", "0.0.0.0:3305", "10.10.121.219:3306");
+			Proxy proxy = toxiproxyClient().createProxy("mysql", "0.0.0.0:3305", "192.168.1.2:3306");
 			intoxicate(proxy);
 			return proxy;
 		}
