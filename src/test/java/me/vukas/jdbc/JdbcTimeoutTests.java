edfe16659ed53529a920aea1d682b1952f04a7d4
Bojan Vukasovic <b.vukasovic@sportradar.com>
2018-10-26 12:01:23 +0200

    fixed containers. needs env variable DOCKER_HOST=tcp://10.10.121.219:2376

--- src/test/java/me/vukas/jdbc/JdbcTimeoutTests.java
+++ src/test/java/me/vukas/jdbc/JdbcTimeoutTests.java
@@ -1,30 +1,22 @@
 package me.vukas.jdbc;
 
-import static java.time.temporal.ChronoUnit.SECONDS;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
-import java.time.Duration;
-import java.time.temporal.TemporalUnit;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 import eu.rekawek.toxiproxy.Proxy;
 import eu.rekawek.toxiproxy.model.ToxicDirection;
 
-import org.junit.ClassRule;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.RecoverableDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.containers.wait.strategy.HostPortWaitStrategy;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ToxiProxyConfig.class)
@@ -38,17 +30,7 @@ public class JdbcTimeoutTests {
 	@Autowired
 	private Proxy mysqlProxy;
 
-	@ClassRule
-	public static MySQLContainer mysql = new MySQLContainer()
-			.withUsername("root")
-			.withPassword("123456");
-
-	@ClassRule
-	public static GenericContainer toxiProxy =
-			new GenericContainer("shopify/toxiproxy")
-					.withExposedPorts(8474);
-
-	@Test
+	@Test(timeout = 120_000, expected = RecoverableDataAccessException.class)
 	public void readTimeout(){
 
 		executor.schedule(() -> {
--- src/test/java/me/vukas/jdbc/ToxiProxyConfig.java
+++ src/test/java/me/vukas/jdbc/ToxiProxyConfig.java
@@ -8,23 +8,49 @@ import java.io.IOException;
 import eu.rekawek.toxiproxy.Proxy;
 import eu.rekawek.toxiproxy.ToxiproxyClient;
 
-import org.junit.ClassRule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.DependsOn;
+import org.testcontainers.containers.FixedHostPortGenericContainer;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.containers.wait.strategy.Wait;
 
 @TestConfiguration
 public class ToxiProxyConfig {
 
+		private static final Logger logger = LoggerFactory.getLogger(ToxiProxyConfig.class);
+
+		@Bean(initMethod = "start", destroyMethod = "stop")
+		public MySQLContainer mysqlContainer() {
+			return new MySQLContainer()
+					.withPassword("123456");
+		}
+
+		@Bean(initMethod = "start", destroyMethod = "stop")
+		@DependsOn("mysqlContainer")
+		public GenericContainer toxiProxyContainer() {
+			return new FixedHostPortGenericContainer("shopify/toxiproxy")
+					.withFixedExposedPort(3305, 3305)
+					.withFixedExposedPort(8474, 8474)
+					.withExposedPorts(8474, 3305)
+					.withLogConsumer(new Slf4jLogConsumer(logger))
+					.waitingFor(Wait.forLogMessage(".*8474.*\\n",1));
+		}
+
 		@Bean
+		@DependsOn("toxiProxyContainer")
 		public ToxiproxyClient toxiproxyClient(){
-			return new ToxiproxyClient("192.168.1.2", 8474);
+			return new ToxiproxyClient("10.10.121.219", 8474);
 		}
 
 		@Bean
 		public Proxy mysqlProxy() throws IOException {
-			Proxy proxy = toxiproxyClient().createProxy("mysql", "0.0.0.0:3305", "192.168.1.2:3306");
+			Proxy proxy = toxiproxyClient().createProxy("mysql", "0.0.0.0:3305",
+					mysqlContainer().getContainerIpAddress()+":"+mysqlContainer().getMappedPort(3306));
 			intoxicate(proxy);
 			return proxy;
 		}
